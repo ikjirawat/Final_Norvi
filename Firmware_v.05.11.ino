a3d464ef5e90937f320a6f9729f2179ef626dcee
IkJirawat <hakimulaake@hotmail.com>
2021-10-27 16:59:12 +0700

    First commit

---
+++ Firmware_v.05.11.ino
@@ -0,0 +1,476 @@
+#include <Arduino.h>
+#include "esp_system.h"
+
+#include <Wire.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+#include <SPIFFS.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>   ////OTA
+
+//// NorVi Session ////
+
+#include "SSD1306.h"
+#define ANALOG_PIN_0  32
+SSD1306  display(0x3C, 16, 17);
+int analog_value = 0;
+String adcString;
+
+//// END NorVi Session ////
+
+char data[100];
+char datart[100];
+char datact[100];
+char dataev[100];
+
+//Wi-Fi User name & Password: IoT & BIT210821k
+const char* ssid     = "Guest"; //Guest ---- I4.0
+const char* password = "BIR830741y"; //BIR830741y ---- factoryI4.0
+
+//MQTT Broker IP 192.168.74.72  //13.10.0.87 ///for test: broker.hivemq.com
+
+const char* mqtt_server = "broker.hivemq.com";
+
+String mcName = "m06";
+String mcLoca = "R2";
+String mcBuilding = "R2";
+
+#define mqtt_user "" //beltonoee
+#define mqtt_pass "" //beltonoee
+#define mqtt_port 1883
+#define mqtt_topic "test"
+#define mqtt_runtime "R2/rt/m06"
+#define mqtt_downtime "R2/idlt/m06"
+#define mqtt_counting "R2/ct/m06"
+#define mqtt_event_runtime "R2/ev/m06"
+#define willTopic "R2/st/m06"
+#define mqtt_json "R2/json/m06"
+
+char* willMessage = "disconnect MQTT";
+
+AsyncWebServer server(80);
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+/////////////////////////////////////////////////////////////
+
+//uint64_t macAddress = ESP.getEfuseMac();
+//uint64_t macAddressTrunc = macAddress << 40;
+//chipID = macAddressTrunc >> 40;
+
+char serialNo[16];
+uint64_t chipid = ESP.getEfuseMac(); // The chip ID is essentially its MAC address(length: 6 bytes).
+uint16_t chip = (uint16_t)(chipid >> 32);
+
+const int buttonPin = 33;   //I.2 runtime
+const int counterpin = 22; //I.6 counting
+
+const int ledstate = 27;
+const int ledmqstate = 23;
+
+int currentstate = 0;
+int previousstate = 0;
+int evcurstate = 0;
+
+int counting;
+
+int rrtstate = 0;
+
+int dtState = 0;
+int rtState = 0;
+
+///used insted of dalay function
+const long ctTime = 10000;
+unsigned long preTime = 0;
+
+unsigned long previousMillis = 0;
+const long interval = 5000;
+
+unsigned long preHigh = 0;
+unsigned long preLow = 0;
+const long intervalHigh = 1000;
+const long intervalLow = 6000;
+
+int evrt = 0;
+int evdt = 0;
+const int evinterval = 1000;
+int evpreT = 0;
+int evstate = 0;
+
+String statusCheckRT;
+String receieveFromIP = "";
+
+void callback(char* topic, byte * payload, unsigned int length) {
+  Serial.println("-------new message from broker-----");
+  Serial.print("channel:");
+  Serial.println(topic);
+  Serial.print("data:");
+  Serial.write(payload, length);
+  Serial.println();
+}
+
+int checkState() {
+  int buttonState = 0;
+  int rtState = 0;
+  int ZState = 0;
+  buttonState = digitalRead(buttonPin); //pin 15
+  //  rtState = digitalRead(runTimepin); //pin 27
+  //Serial.println(buttonState);
+  if (buttonState == LOW) {
+    ZState = 1;
+  }
+  else if (buttonState == HIGH) {
+    ZState = 0;
+  }
+  return ZState;
+}
+
+void run_time_state() {            //// auto sending loop used instead of delay function rt&dt
+  unsigned long crTime = millis();
+  if (crTime - preTime >= ctTime) {
+    preTime = crTime;
+    rrtstate = digitalRead(buttonPin); // I.2 runtime
+    int rttstate = checkState();
+    sprintf(datart, "%d", rttstate);
+    client.publish(mqtt_runtime, datart);
+    publishData();
+    if (rttstate = 1) {
+      statusCheckRT = "stop";
+      update_display();
+    }
+    if (rttstate = 0) {
+      statusCheckRT = "running";
+      update_display();
+    }
+    update_display();
+  }
+}
+
+void counter_state () {
+  counting = 0;
+  currentstate = digitalRead(counterpin); // I.6 counting
+  unsigned long currenT = millis();
+
+  if (currentstate == LOW && currenT - preHigh >= intervalHigh && previousstate == 0) {
+    counting = 1;
+    Serial.print("Counter:");
+    Serial.println(counting);
+    sprintf(datact, "%d", counting);
+    client.publish(mqtt_counting, datact);
+    publishData();
+    previousstate = 1;
+    preHigh = currenT;
+    preLow = preHigh;
+  }
+  else if (currentstate == HIGH && currenT - preLow >= intervalLow && previousstate == 1) {
+    previousstate = 0;
+    preLow = currenT;
+  }
+}
+
+int writeToFile(String filename, String strtxt) {
+  File file = SPIFFS.open("/setting.json", "w");
+  if (!file) {
+    Serial.println("Error opening file for writing");
+    return (0);
+  }
+  int bytesWritten = file.println(strtxt);
+  file.close();
+  return (bytesWritten);
+}
+
+String readFile(String filename) {
+  String str = "";
+  char ch;
+  File file = SPIFFS.open("/setting.json", "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return (str);
+  }
+
+  while (file.available()) {
+    ch = file.read();
+    if (ch != 0x0a) {
+      str += ch;
+    }
+  }
+  file.close();
+  //     str.trim();
+  return (str);
+}
+
+
+void Write_parameter_on_SPIFFS() {
+  //  int i = 0;
+  //  String strIP = "";
+  //
+  //  i = writeToFile("setting.json", receieveFromIP);
+  //  if (i > 0) {
+  //    Serial.println("write file successed " + String(i) + " bytes");
+  //
+  //    strIP = readFile("setting.json");
+  //    if (strIP != "") {
+  //      Serial.print("SPIFFS Content: ");
+  //      Serial.println(strIP);
+  //      Serial.println(strIP.length());
+  //    }
+  //  }
+}
+
+
+void Setting_parameter_on_web_server() {
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(SPIFFS, "/index.html", "text/html", false);
+  });
+
+  server.on("/espReset", HTTP_GET, [](AsyncWebServerRequest * request) {
+    ESP.restart();
+    request->send(200, "text/plain", "ESP32 has been reset!");
+  });
+
+  server.on("/showpara", HTTP_GET, [](AsyncWebServerRequest * request) {
+    String str = "";
+    int paramsNr = request->params();
+    str = "";
+
+    int wf = 0;
+    String strIP = "";
+
+    //    for (int i = 0; i < paramsNr; i++) {
+    //      DynamicJsonDocument  doc(1024);
+    //      JsonObject pconvert =  doc.to<JsonObject>();;
+    //
+    //      AsyncWebParameter* p = request->getParam(i);
+    //
+    //      pconvert[p->name()] = p->value();
+    //
+    //      str += p->name();
+    //      str += ":";
+    //      str += p->value() + "\r\n";
+    //
+    //      serializeJson(doc, bufSend);
+    //      Serial.println(bufSend);
+    //    }
+    //    Serial.println(str);
+    //    str += "\n\n";
+    /////////////////////////////////Path_add/////////////////////////////////////
+    DynamicJsonDocument  doc(1024);
+    JsonObject pconvert =  doc.to<JsonObject>();;
+
+    AsyncWebParameter* p = request->getParam(0);
+    AsyncWebParameter* p1 = request->getParam(1);
+    AsyncWebParameter* p2 = request->getParam(2);
+    AsyncWebParameter* p3 = request->getParam(3);
+    AsyncWebParameter* p4 = request->getParam(4);
+    AsyncWebParameter* p5 = request->getParam(5);
+    AsyncWebParameter* p6 = request->getParam(6);
+    AsyncWebParameter* p7 = request->getParam(7);
+    AsyncWebParameter* p8 = request->getParam(8);
+
+    pconvert[p->name()] = p->value();
+    pconvert[p1->name()] = p1->value();
+    pconvert[p2->name()] = p2->value();
+    pconvert[p3->name()] = p3->value();
+    pconvert[p4->name()] = p4->value();
+    pconvert[p5->name()] = p5->value();
+    pconvert[p6->name()] = p6->value();
+    pconvert[p7->name()] = p7->value();
+    pconvert[p8->name()] = p8->value();
+
+    char bufSend[300];
+    serializeJson(doc, bufSend);
+    //    Serial.println(bufSend);
+
+    wf = writeToFile("setting.json", bufSend); //////Write to SPIFFS
+    set_parameter_from_SPIFFS();
+//    if (wf > 0) {
+//      Serial.println("write file successed " + String(wf) + " bytes");
+//
+//      strIP = readFile("setting.json");  //////Read from SPIFFS
+//      if (strIP != "") {
+//        Serial.print("SPIFFS Content: ");
+//        Serial.println(strIP);
+//        Serial.println(strIP.length());
+//      }
+//    }
+    /////////////////////////////////Path_end/////////////////////////////////////
+    request->send(200, "text/plain", "message received\n\n" + String(str));
+  });
+
+}
+
+void set_parameter_from_SPIFFS() {
+  String read_spiffs = "";
+  read_spiffs = readFile("setting.json");  //////Read from SPIFFS
+  if (read_spiffs != "") {
+    Serial.print("SPIFFS Content: ");
+    Serial.println(read_spiffs);
+    Serial.println(read_spiffs.length());
+  }
+}
+
+
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Connected to AP successfully!");
+}
+
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info) {
+  Serial.println("Disconnected from WiFi access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.disconnected.reason);
+  Serial.println("Trying to Reconnect");
+  WiFi.begin(ssid, password);
+}
+
+void device_restart() {
+  analog_value = analogRead(ANALOG_PIN_0);
+  //  Serial.println(analog_value);
+  int pretimer = 0;
+  int dummy = 0;
+  int justcheck = 0;
+  const long targettimer = 1000;
+  unsigned long crtimer = millis();
+  if (3300 > analog_value && analog_value > 2650) {
+    justcheck = 1;
+  }
+  if (crtimer - pretimer >= targettimer && justcheck == 1) {
+    pretimer = crtimer;
+    dummy = dummy + 1;
+    ESP.restart();
+  }
+  else {
+    justcheck = 0;
+    dummy = 0;
+  }
+}
+
+
+void setup() {
+  snprintf(serialNo, sizeof(serialNo), "SN:m06-%04X%08X", chip, (uint32_t)chipid);
+  Serial.begin(115200);
+
+  bool success = SPIFFS.begin();
+
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.drawLogBuffer(0, 0);
+  display.display();
+
+  pinMode(ledstate, OUTPUT);
+  pinMode(ledmqstate, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(counterpin, INPUT_PULLUP);
+
+  //  digitalWrite(buttonPin,HIGH);
+  //  digitalWrite(counterpin,HIGH);
+
+  if (!SPIFFS.begin()) {
+    Serial.println("Error while mounting SPIFFS");
+    return;
+  }
+
+  WiFi.disconnect(true);
+  delay(100);
+  WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
+  WiFi.onEvent(WiFiGotIP, SYSTEM_EVENT_STA_GOT_IP);
+  WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    digitalWrite(ledstate, HIGH);
+    delay(100);
+    digitalWrite(ledstate, LOW);
+    delay(1000);
+  }
+//  Serial.println("");
+//  Serial.println("WiFi connected");
+//  Serial.println("IP address: ");
+//  Serial.println(WiFi.localIP());
+
+  update_display();
+
+  //----------------------- MQTT -----------------------//
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  //----------------------- MQTT -----------------------//
+
+  Setting_parameter_on_web_server();
+  AsyncElegantOTA.begin(&server);   ////OTA
+  server.begin();
+}
+
+
+void loop() {
+  AsyncElegantOTA.loop();   ////OTA
+
+  if (!client.connected()) {
+    if (millis() - previousMillis > interval ) {
+      previousMillis = millis();
+      Serial.print("MQTT connection... ");
+      String clientId = String() + "CNCm06R2-" + String(serialNo) + "-";
+      clientId += String(random(0xffff), HEX);
+      if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass, willTopic, 0, true, willMessage))  {
+        client.publish(willTopic, willMessage, true);
+        Serial.println("Connected");
+      }
+      else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+      }
+    }
+  }
+  counter_state();
+  run_time_state();
+  update_display();
+  client.loop();
+}
+
+void update_display() {   //  display.drawString(0, 0, "CNC-m06");
+  display.clear();
+
+  String LocalIP = String() + "IP:" + WiFi.localIP()[0] + "." + WiFi.localIP()[1] + "." + WiFi.localIP()[2] + "." + WiFi.localIP()[3];
+  display.drawString(0, 0, LocalIP);
+
+  adcString = String(serialNo);
+  display.drawString(0, 15, adcString);
+
+  String WiFiRSSI = String() + "RSSI:" + WiFi.RSSI();
+  display.drawString(0, 30, WiFiRSSI);
+
+  String StChk = String() + "Status:" + statusCheckRT;
+  display.drawString(0, 45, StChk);
+
+  adcString = String();
+  display.drawString(30, 45, adcString);
+
+  display.display();
+}
+
+void publishData() {
+  String LocalIP = String() + "IP:" + WiFi.localIP()[0] + "." + WiFi.localIP()[1] + "." + WiFi.localIP()[2] + "." + WiFi.localIP()[3];
+  DynamicJsonDocument  doc(1024);
+  JsonObject control =  doc.to<JsonObject>();;
+  control["mcName"] = mcName;
+  control["location"] = mcLoca;
+  control["deviceIP"] = LocalIP;
+  control["deviceSN"] = String(serialNo);
+  JsonObject objData = doc.createNestedObject("data");
+  objData["runtime"] = datart;
+  objData["counter"] = datact;
+  objData["event"] = dataev;
+  objData["mcstatus"] = statusCheckRT;
+  control["RSSI"] = WiFi.RSSI();
+  char bufferSend[300] ;
+  serializeJson(doc, bufferSend);
+  client.publish(mqtt_json, bufferSend, true);
+}
